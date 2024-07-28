9ba0610fb0c27aee72012da2bb362a79322adfee
Blake Collins <blakecollins78@gmail.com>
2024-07-28 10:39:37 +0000

    NOTE: cd into myshop to run. ADD: add first products

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# NEW BS
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
